2fe71320aad2dca527f54e8a8fac3290c24c37e9
Martin Kjellin <makj3271@student.uu.se>
2015-05-28 10:36:33 +0200

    Test of plotting.

--- testplot.py
+++ testplot.py
@@ -1,7 +1,4 @@
-import matplotlib.pyplot as plot
 from matplotlib import pyplot
-all_data = [[1,2],[2,5]]
-for point in all_data:
-	for point2 in all_data:
-		pyplot.plot([point[0], point2[0]], [point[1], point2[1]])
+
+pyplot.plot([0, 200], [300, 0])
 pyplot.show() 
