4df3ffa46b77e5f7e692ae54dcb51958a6ac1a6a
Ubuntu <ubuntu@host-10-0-100-123.openstacklocal>
2015-05-28 10:23:56 +0000

    Plot finally working!

--- testspark.py
+++ testspark.py
@@ -1,11 +1,9 @@
 import os
 import sys
 from pyspark import SparkContext
+from matplotlib import pyplot
 
 sc = SparkContext(appName = 'genes', master = os.environ['MASTER'])
-
-#def lenlimit(line):
-#        return abs(int(line[10])) > 1000
 
 path = sys.argv[1]
 fileNames = ['HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.sam'] # Should get all filenames in the directory in our final solution
@@ -17,24 +15,15 @@ for fileName in fileNames:
 	lines = sc.textFile(path + '/' + fileName)
 
 	fullLines = lines.map(lambda line: [individual, country] + line.split())
-	filteredLines = fullLines.filter(lambda line: abs(int(line[10])) > 1000) #lenlimit
+	filteredLines = fullLines.filter(lambda line: abs(int(line[10])) > 1000)
 
 	# Doing a collect here might be unnecessary
 	groupedLines = filteredLines.groupBy(lambda line: line[2]).collect()
 	unsortedLines = [(id, list(fragments)) for (id, fragments) in groupedLines]
 
-	plotData = [([0, reads[1][5]], [reads[0][5], 0]) for (_, reads) in groupedLines]
+	plotData = [([0, int(reads[1][5])], [int(reads[0][5]), 0]) for (_, reads) in unsortedLines]
 
-	print plotData
-#		firstLocation = reads[0][5]
-#		secondLocation = reads[1][5]
-#		print firstLocation + ", " + secondLocation
-
-	# print groupedLines
-
-#	unsortedLines = [(id, list(fragments)) for (id, fragments) in groupedLines]
-	#	print unsortedLines
-
-#	sortedLines = sorted([(id, sorted(fragments)) for (id, fragments) in groupedLines])
-#	print sortedLines
-#	print len(sortedLines)
+	for (xData, yData) in plotData:
+		pyplot.plot(xData, yData)
+	
+	pyplot.show()
