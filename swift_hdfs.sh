f5057bc7fdbcf7154523cc2c893cbb9e137dcca0
Ubuntu <ubuntu@host-10-0-100-126.openstacklocal>
2015-06-04 15:52:58 +0000

    Added a script for streaming from Swift/BAM to HDFS/SAM.

---
+++ swift_hdfs.sh
@@ -0,0 +1,8 @@
+# Download every 25th BAM file from Swift, convert them into SAM format,
+# and store them in HDFS.
+
+BAMFILES=$(swift list GenomeData | grep -v .bam. | awk 'NR % 1000 == 0')
+for BAMFILE in $BAMFILES; do
+  SAMFILE=$(echo $BAMFILE | sed 's/.bam/.sam/g')
+  swift download GenomeData $BAMFILE --output - | samtools view - | hdfs dfs -put - /seqdata/$SAMFILE
+done
