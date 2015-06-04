feedaf39157907c87cfee91f68c4d664ca874d91
Ubuntu <ubuntu@host-10-0-100-134.openstacklocal>
2015-06-04 17:49:55 +0000

    Added conversion from country to continent.

--- testspark.py
+++ testspark.py
@@ -6,7 +6,9 @@ from matplotlib import pyplot
 sc = SparkContext(appName = 'genes', master = os.environ['MASTER'])
 
 path = sys.argv[1]
-fileNames = ['HG00096.chrom20.ILLUMINA.bwa.GBR.low_coverage.20120522.sam'] # Should get all filenames in the directory in our final solution
+fileNames = ['HG00122.chrom20.ILLUMINA.bwa.GBR.low_coverage.20121211.sam'] # Should get all filenames in the directory in our final solution
+
+countryDictionary = {'CDX': 'EAS', 'CHB': 'EAS', 'JPT': 'EAS', 'KHV': 'EAS', 'CHS': 'EAS', 'BEB': 'SAS', 'GIH': 'SAS', 'ITU': 'SAS', 'PJL': 'SAS', 'STU': 'SAS', 'ASW': 'AFR', 'ACB': 'AFR', 'ESN': 'AFR', 'GWD': 'AFR', 'LWK': 'AFR', 'MSL': 'AFR', 'YRI': 'AFR', 'GBR': 'EUR', 'FIN': 'EUR', 'IBS': 'EUR', 'TSI': 'EUR', 'CEU': 'EUR', 'CLM': 'AMR', 'MXL': 'AMR', 'PEL': 'AMR', 'PUR': 'AMR'}
 
 for fileName in fileNames:
 	fileNameParts = fileName.split('.')
@@ -14,16 +16,18 @@ for fileName in fileNames:
 	country = fileNameParts[4]
 	lines = sc.textFile(path + '/' + fileName)
 
-	fullLines = lines.map(lambda line: [individual, country] + line.split())
+	fullLines = lines.map(lambda line: [individual, countryDictionary[country]] + line.split())
 	filteredLines = fullLines.filter(lambda line: abs(int(line[10])) > 1000)
 
 	# Doing a collect here might be unnecessary
 	groupedLines = filteredLines.groupBy(lambda line: line[2]).collect()
 	unsortedLines = [(id, list(fragments)) for (id, fragments) in groupedLines]
 
-	plotData = [([0, int(reads[1][5])], [int(reads[0][5]), 0]) for (_, reads) in unsortedLines]
+	print unsortedLines
 
-	for (xData, yData) in plotData:
-		pyplot.plot(xData, yData)
+#	plotData = [([0, int(reads[1][5])], [int(reads[0][5]), 0]) for (_, reads) in unsortedLines]
+
+#	for (xData, yData) in plotData:
+#		pyplot.plot(xData, yData)
 	
-	pyplot.show()
+#	pyplot.show()
