6cc4d9da96938505b6b5f368863d9e56c863049c
Ubuntu <ubuntu@host-10-0-100-134.openstacklocal>
2015-06-05 16:50:54 +0000

    Working version?

--- swift_hdfs.sh
+++ swift_hdfs.sh
@@ -1,7 +1,7 @@
 # Download every 25th BAM file from Swift, convert them into SAM format,
 # and store them in HDFS.
 
-BAMFILES=$(swift list GenomeData | grep -v .bam. | awk 'NR % 1000 == 0')
+BAMFILES=$(swift list GenomeData | grep -v .bam. | awk 'NR % 25 == 0')
 for BAMFILE in $BAMFILES; do
   SAMFILE=$(echo $BAMFILE | sed 's/.bam/.sam/g')
   swift download GenomeData $BAMFILE --output - | samtools view - | hdfs dfs -put - /seqdata/$SAMFILE
--- testspark.py
+++ testspark.py
@@ -6,28 +6,22 @@ from matplotlib import pyplot
 sc = SparkContext(appName = 'genes', master = os.environ['MASTER'])
 
 path = sys.argv[1]
-fileNames = ['HG00122.chrom20.ILLUMINA.bwa.GBR.low_coverage.20121211.sam'] # Should get all filenames in the directory in our final solution
 
 countryDictionary = {'CDX': 'EAS', 'CHB': 'EAS', 'JPT': 'EAS', 'KHV': 'EAS', 'CHS': 'EAS', 'BEB': 'SAS', 'GIH': 'SAS', 'ITU': 'SAS', 'PJL': 'SAS', 'STU': 'SAS', 'ASW': 'AFR', 'ACB': 'AFR', 'ESN': 'AFR', 'GWD': 'AFR', 'LWK': 'AFR', 'MSL': 'AFR', 'YRI': 'AFR', 'GBR': 'EUR', 'FIN': 'EUR', 'IBS': 'EUR', 'TSI': 'EUR', 'CEU': 'EUR', 'CLM': 'AMR', 'MXL': 'AMR', 'PEL': 'AMR', 'PUR': 'AMR'}
 
-for fileName in fileNames:
+def splitFile((fileName, fileContent)):
 	fileNameParts = fileName.split('.')
-	individual = fileNameParts[0]
-	country = fileNameParts[4]
-	lines = sc.textFile(path + '/' + fileName)
+	country = fileNameParts[5]
+        continent = countryDictionary[country]
+        lines = fileContent.strip().split('\n')
+        return [(continent, line.split()) for line in lines]
 
-	fullLines = lines.map(lambda line: [individual, countryDictionary[country]] + line.split())
-	filteredLines = fullLines.filter(lambda line: abs(int(line[10])) > 1000)
+files = sc.wholeTextFiles(path)
+splitFiles = files.flatMap(splitFile)
+totalCounts = splitFiles.countByKey()
 
-	# Doing a collect here might be unnecessary
-	groupedLines = filteredLines.groupBy(lambda line: line[2]).collect()
-	unsortedLines = [(id, list(fragments)) for (id, fragments) in groupedLines]
+filteredLines = splitFiles.filter(lambda (_, line): abs(int(line[8])) > 1000)
+filteredCounts = filteredLines.countByKey()
 
-	print unsortedLines
-
-#	plotData = [([0, int(reads[1][5])], [int(reads[0][5]), 0]) for (_, reads) in unsortedLines]
-
-#	for (xData, yData) in plotData:
-#		pyplot.plot(xData, yData)
-	
-#	pyplot.show()
+for continent, filteredCount in filteredCounts.iteritems():
+        print continent + ": " + str(float(filteredCount) / totalCounts[continent])
