429ac19bc00bb92441e7fabf8dcd8e266dd2b60a
Ubuntu <ubuntu@host-10-0-100-134.openstacklocal>
2015-06-08 23:45:39 +0000

    Finally fully working!

--- testspark.py
+++ testspark.py
@@ -9,18 +9,30 @@ path = sys.argv[1]
 
 countryDictionary = {'CDX': 'EAS', 'CHB': 'EAS', 'JPT': 'EAS', 'KHV': 'EAS', 'CHS': 'EAS', 'BEB': 'SAS', 'GIH': 'SAS', 'ITU': 'SAS', 'PJL': 'SAS', 'STU': 'SAS', 'ASW': 'AFR', 'ACB': 'AFR', 'ESN': 'AFR', 'GWD': 'AFR', 'LWK': 'AFR', 'MSL': 'AFR', 'YRI': 'AFR', 'GBR': 'EUR', 'FIN': 'EUR', 'IBS': 'EUR', 'TSI': 'EUR', 'CEU': 'EUR', 'CLM': 'AMR', 'MXL': 'AMR', 'PEL': 'AMR', 'PUR': 'AMR'}
 
-def splitFile((fileName, fileContent)):
-	fileNameParts = fileName.split('.')
-	country = fileNameParts[5]
-        continent = countryDictionary[country]
-        lines = fileContent.strip().split('\n')
-        return [(continent, line.split()) for line in lines]
+# Get a list of all SAM files in the directory
+fileFile = open(path + '.txt', 'r') # Smarter way to get list of files?
+files = fileFile.readlines()
+fileFile.close()
 
-files = sc.wholeTextFiles(path)
-splitFiles = files.flatMap(splitFile)
-totalCounts = splitFiles.countByKey()
+totalLines = sc.parallelize([])
+print "********************************************************************"
+print "START PARTITIONS: " + str(totalLines.getNumPartitions())
+print "********************************************************************"
 
-filteredLines = splitFiles.filter(lambda (_, line): abs(int(line[8])) > 1000)
+for file in files:
+    fileNameParts = file[:-1].split('.')
+    country = fileNameParts[4]
+    lines = sc.textFile('/' + path + '/' + file[:-1])
+    print "********************************************************************"
+    print "ADDED PARTITIONS " + file + ": " + str(lines.getNumPartitions())
+    print "********************************************************************"
+
+    continentLines = lines.map(lambda line: (countryDictionary[country], line.split()))
+    totalLines = totalLines.union(continentLines)
+
+totalCounts = totalLines.countByKey()
+
+filteredLines = totalLines.filter(lambda (_, line): abs(int(line[8])) > 1000)
 filteredCounts = filteredLines.countByKey()
 
 for continent, filteredCount in filteredCounts.iteritems():
