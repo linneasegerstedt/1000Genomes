18bed9b9236a25c20344a2b376ee7cabb0cb6c75
Ubuntu <ubuntu@host-10-0-100-134.openstacklocal>
2015-06-09 17:23:01 +0000

    Location data (for use in histograms) are now produced. Some clean-up of old files made.

--- testplot.py
+++
@@ -1,8 +0,0 @@
-from matplotlib import pyplot
-
-xData = ([0, 200], [0, 350])
-yData = ([300, 0], [250, 0])
-
-for x, y in xData, yData:
-  pyplot.plot(x, y)
-  pyplot.show() 
--- testspark.py
+++ testspark.py
@@ -1,39 +1,62 @@
 import os
 import sys
 from pyspark import SparkContext
-from matplotlib import pyplot
+import pickle
 
 sc = SparkContext(appName = 'genes', master = os.environ['MASTER'])
-
 path = sys.argv[1]
 
 countryDictionary = {'CDX': 'EAS', 'CHB': 'EAS', 'JPT': 'EAS', 'KHV': 'EAS', 'CHS': 'EAS', 'BEB': 'SAS', 'GIH': 'SAS', 'ITU': 'SAS', 'PJL': 'SAS', 'STU': 'SAS', 'ASW': 'AFR', 'ACB': 'AFR', 'ESN': 'AFR', 'GWD': 'AFR', 'LWK': 'AFR', 'MSL': 'AFR', 'YRI': 'AFR', 'GBR': 'EUR', 'FIN': 'EUR', 'IBS': 'EUR', 'TSI': 'EUR', 'CEU': 'EUR', 'CLM': 'AMR', 'MXL': 'AMR', 'PEL': 'AMR', 'PUR': 'AMR'}
 
-# Get a list of all SAM files in the directory
-fileFile = open(path + '.txt', 'r') # Smarter way to get list of files?
+
+### For each "continent" (group of populations), find the share of fragments ###
+### for which the alignment indicates a length of more than 1000 base pairs. ###
+
+# Read a list of all SAM files in the directory. Presupposes that there is a file
+# called <path>.txt which contains such a list.  
+fileFile = open(path + '.txt', 'r')
 files = fileFile.readlines()
 fileFile.close()
 
+outputFile = open(path + '-output.txt', 'wb')
+
 totalLines = sc.parallelize([])
-print "********************************************************************"
-print "START PARTITIONS: " + str(totalLines.getNumPartitions())
-print "********************************************************************"
+outputFile.write('Initial number of partitions: ' + str(totalLines.getNumPartitions()) + '\n\n')
 
 for file in files:
-    fileNameParts = file[:-1].split('.')
+    fileName = file[:-1] # Take away newline character
+    fileNameParts = fileName.split('.')
     country = fileNameParts[4]
-    lines = sc.textFile('/' + path + '/' + file[:-1])
-    print "********************************************************************"
-    print "ADDED PARTITIONS " + file + ": " + str(lines.getNumPartitions())
-    print "********************************************************************"
+    lines = sc.textFile('/' + path + '/' + fileName)
+    outputFile.write('Added number of partitions for ' + fileName + ': ' + str(lines.getNumPartitions()) + '\n\n')
 
     continentLines = lines.map(lambda line: (countryDictionary[country], line.split()))
     totalLines = totalLines.union(continentLines)
 
-totalCounts = totalLines.countByKey()
-
 filteredLines = totalLines.filter(lambda (_, line): abs(int(line[8])) > 1000)
-filteredCounts = filteredLines.countByKey()
 
+totalCounts = totalLines.countByKey()
+filteredCounts = filteredLines.countByKey()
 for continent, filteredCount in filteredCounts.iteritems():
-        print continent + ": " + str(float(filteredCount) / totalCounts[continent])
+    outputFile.write(continent + '\n')
+    outputFile.write('Total count: ' + str(totalCounts[continent]) + '\n')
+    outputFile.write('Filtered count: ' + str(filteredCount) + '\n')
+    outputFile.write('Share: ' + str(float(filteredCount) / totalCounts[continent]) + '\n\n')
+
+outputFile.close()
+
+
+### Group the locations of reads by "continent" (group of populations) ###
+
+def seqFunc(locations, line):
+    locations.append(int(line[3]))
+    return locations
+
+def combFunc(firstLocations, secondLocations):
+    return firstLocations + secondLocations
+
+locationsByContinent = filteredLines.aggregateByKey([], seqFunc, combFunc).collect()
+
+pickleFile = open(path + '-pickle.txt', 'wb')
+pickle.dump(locationsByContinent, pickleFile)
+pickleFile.close()
